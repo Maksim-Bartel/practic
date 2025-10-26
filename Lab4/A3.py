e0a2242a7c157b963f698c2dfce8adad71d52186
Максим Бартель <mkcmbartel907@gmail.com>
2025-10-26 18:14:30 +0300

    Add files via upload

---
+++ Lab4/A3.py
@@ -0,0 +1,32 @@
+packets = input()
+lost_pack = 0
+max_lost_streak = 0
+current_streak = 0
+if len(packets) < 5:
+    print('Длина последовательности должна быть не меньше 5')
+    exit(0)
+if not all(char in '01' for char in packets):
+    print('Неверный ввод. Используйте только символы "0" и "1"')
+    exit(0)
+for packet in packets:
+        if packet == '0':
+            lost_pack += 1
+            current_streak += 1
+            max_lost_streak = max(max_lost_streak, current_streak)
+        else:current_streak = 0
+loss_percentage = (lost_pack / len(packets)) * 100
+if loss_percentage <=1:
+    quality = 'Отличное качество'
+elif loss_percentage <= 5:
+    quality = 'Хорошее качество'
+elif loss_percentage <= 10:
+    quality = 'Удовлетворительное качество'
+elif loss_percentage <= 20:
+    quality = 'Плохое качество'
+else:
+    quality = 'Критическое состояние сети'
+print('Общее количество пакетов:', len(packets))
+print('Количество потерянных пакетов:', packets.count('0'))
+print('Длина самой длинной последовательности потерянных пакетов:', max_lost_streak)
+print(f'Процент потерь: {lost_pack / len(packets) * 100}%')
+print('Качество связи:', quality)
---
+++ Lab4/A4 .py
@@ -0,0 +1,42 @@
+import re
+
+
+def IsValidNumber(cardNumber):
+    return cardNumber.isdigit() and len(cardNumber) in [13, 15, 16]
+
+# я эту функцию (getCheckSum) почти полностью поменял
+def getCheckSum(cardNumber):
+    checkSum = 0
+    for i, char in enumerate(reversed(cardNumber)):
+        digit = int(char)
+        if i % 2 == 1:
+            digit *= 2
+            if digit > 9:
+                digit -= 9
+        checkSum += digit
+    return checkSum
+
+
+def isValidCheckSum(checkSum):
+    return checkSum % 10 == 0
+
+
+def getCardType(string):
+    if len(string) == 13 or len(string) == 16 and string.startswith("4"):
+        return "Visa"
+    if len(string) == 15 and string.startswith("34") or string.startswith("37"):
+        return "American Express"
+    if len(string) == 16 and re.match(r'5[1-5]', string[:2]):
+        return "Master Card"
+    else:
+        return "Invalid"
+
+
+cardNumber = input("Enter Card Number: ")
+if IsValidNumber(cardNumber):
+    if isValidCheckSum(getCheckSum(cardNumber)):
+        print("Valid")
+        print(getCardType(cardNumber))
+
+else:
+    print("Wrong Input. Try again...")
