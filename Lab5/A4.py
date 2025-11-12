bc36ae44587f0771ff55d10fec2f4d8a051a6f99
Максим Бартель <mkcmbartel907@gmail.com>
2025-11-12 18:34:02 +0300

    в  A4.py добавил 1 функцию которая открывает файл , читает построчно, разбивает по табуляции, сохраняет в словарь закрывает файл и  возвращает значение словаря

--- Lab5/A4.py
+++ Lab5/A4.py
@@ -1,1 +1,14 @@
+def read_text (filename):
+
+  text={}
+  file = open(filename, 'r')
+
+  for line in file:
+     part=line.strip().split('\t')
+     if len(part) == 3:
+          protein, organism, sequence = part
+          text[protein]={'organism':organism,'sequence':sequence}
+  file.close()
+  return text
+
 
