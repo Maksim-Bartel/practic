cc6d5249808bfa5364e78628ad7b85b24772ea39
Максим Бартель <mkcmbartel907@gmail.com>
2025-11-16 14:18:37 +0300

    Update A4.py добавил функцию для поиска наиболее встречаемой аминокислоты а также добавил функцию для обработки команд

--- Lab5/A4.py
+++ Lab5/A4.py
@@ -97,6 +97,88 @@ def diff_operation(proteins, protein1,protein2):
             diff += 1
     diff = abs(len(seq1) - len(seq2))
     return str(diff)
+def mode (proteins, protein_name):
+
+    target_protein=None
+
+    for protein in proteins:
+        name, organism, sequence = protein
+        if name == protein_name:
+            target_protein = (name, organism, sequence)
+            break
+
+    if target_protein is None:
+        return "NOT FOUND"
+
+    name, organism , sequence  = target_protein
+
+    counter_amino={}
+
+    for amino in sequence:
+        counter_amino[amino] = counter_amino.get(amino, 0) + 1
+    count=0
+
+    most_amino = ''
+    for amino,counts in counter_amino.items():
+        if counts > count or (counts == count and amino < most_amino):
+            most_amino = amino
+            count = counts
+
+    return (f'{most_amino} {count}')
+
+def process_commands(sequences_file, commands_file, output_file):
+   
+
+    proteins = read_sequences(sequences_file)
+    if not proteins:
+        print("Нет данных для обработки")
+        return
+
+    try:
+        with open(commands_file, 'r', encoding='utf-8') as file:
+            commands = file.readlines()
+    except FileNotFoundError:
+        print(f"Ошибка: файл {commands_file} не найден")
+        return
+
+    with open(output_file, 'w', encoding='utf-8') as out_file:
+        out_file.write("Ваше Имя\n")
+        out_file.write("Генетический поиск\n")
+
+
+        for i, command_line in enumerate(commands, 1):
+            parts = command_line.strip().split('\t')
+            operation = parts[0]
+
+            out_file.write(f"{i:03d}\n")
+            out_file.write("-" * 50 + "\n")
+
+            if operation == "search":
+                if len(parts) >= 2:
+                    pattern = parts[1]
+                    results = search_data(proteins, pattern)
+                    for result in results:
+                        out_file.write(result + "\n")
+
+            elif operation == "diff":
+                if len(parts) >= 3:
+                    protein1 = parts[1]
+                    protein2 = parts[2]
+                    result = diff_operation(proteins, protein1, protein2)
+                    out_file.write(f"amino-acids difference: {result}\n")
+
+            elif operation == "mode":
+                if len(parts) >= 2:
+                    protein_name = parts[1]
+                    result = mode(proteins, protein_name)
+                    out_file.write(f"amino-acid occurs: {result}\n")
+
+            out_file.write("-" * 50 + "\n")
+
+    print(f"Обработка завершена. Результат в файле {output_file}")
+
+process_commands("sequences.txt", "commands.txt", "genedata.0.txt")
+
 
 
 
