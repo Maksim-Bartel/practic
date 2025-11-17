f127b4fd9544b5ca289b4ac3d9fc696017d33787
Максим Бартель <mkcmbartel907@gmail.com>
2025-11-17 17:41:07 +0300

    Update A4.py добавил функцию для чтения файла и функцию для кодирования строки

--- Lab5/A4.py
+++ Lab5/A4.py
@@ -1,3 +1,22 @@
+def read_command(filename):
+    proteins = []
+    file = open(filename, 'r', encoding='utf-8')
+
+    for line in file:
+        parts = line.strip().split('\t')
+        protein_data = (
+            parts[0].strip(),
+            parts[1].strip(),
+            parts[2].strip()
+        )
+        proteins.append(protein_data)
+    return proteins
+
+def decode (werb):
+    for i in range(len(werb)):
+        if werb.isdigit():
+            werb =werb.replace(werb[i],werb[i]*int(werb[i])-1)
+    return werb
 
 
 
