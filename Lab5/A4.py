fde83662de6dc39f69ecb3ca70f7e1f23c0584e2
Максим Бартель <mkcmbartel907@gmail.com>
2025-11-18 09:04:40 +0300

    Update A4.py добавил вывод и подправил некоторые функции : поиска и чтение команд

--- Lab5/A4.py
+++ Lab5/A4.py
@@ -1,5 +1,3 @@
-import string
-import re
 
 file_commands = 'commands.0.txt'
 file_proteins = 'sequences.0.txt'
@@ -24,17 +22,24 @@ def read_commands_data(filename):
 
     for line in file:
        parts=line.strip().split('\t')
-       commands_data= (
+       if parts[0]=='mode' or parts[0]=='search':
+        commands_data= (
          parts[0].strip(),
          parts[1].strip(),
          )
+       else:
+           commands_data = (
+               parts[0].strip(),
+               parts[1].strip(),
+               parts[2].strip()
+           )
        commands.append(commands_data)
     return commands
 
 def decode (werb):
     word=""
     for i in range(len(werb)):
-        if werb.isdigit():
+        if werb[i].isdigit():
             word =word+werb[i+1]*int(werb[i]-1)
         else:
             word=word+werb[i]
@@ -45,35 +50,33 @@ def search_data (protein):
     protein=decode(protein)
     proteins=read_command(file_proteins)
     for i in range(len(proteins)):
-        if (protein[i])[2].find(protein):
-            search=(proteins[i])[1]+" "+ (proteins[i])[2]
-    if search=="":
-        return 'NOT FOUND'
-    else:
-        return search
-def mode (protein):
+        if protein in (proteins[i])[2]:
+            search=(proteins[i])[1]+" "+ (proteins[i])[0]
+    return search
+    if search=="": 
+     return 'NOT FOUND'
     
+
+
+def mode (protein):
     proteins=read_command(file_proteins)
     mode_app=""
-    
     for i in range(len(proteins)):
         if (proteins[0]) == protein:
             mode_app=(proteins[i])[2]
     if protein == "":
         return 'MISSING'
-        
     letters = dict()
     for i in mode_app:
         letters[i]=letters.get(i,0)+1
     letters_app = max(letters,key=letters.get)
     max_ver = letters[letters_app]
-    
     for i in sorted(letters):
-        print(i)
         if letters[i]==max_ver:
               letters_app=i
               break
-    return letters_app,max_ver     
+    return letters_app,max_ver
+
 def diff(protein1, protein2):
     proteins = read_command (file_proteins)
     protein1_1=''
@@ -85,7 +88,7 @@ def diff(protein1, protein2):
         return 'MISSING'
 
     for i in range(len(proteins)):
-            if (proteins[i])[0]==protein2:
+            if (proteins[i])[0]==protein2_2:
                 protein2_2 = (proteins[i])[2]
     if protein2_2=='':
         return 'MISSING'
@@ -104,6 +107,30 @@ def diff(protein1, protein2):
             diff_ans += 1
 
     return diff_ans
+file = open('genedata.txt','w')
+file.write('Bartel Maksim\n')
+file.write('Genetic Searching')
+file.write('-'*50)
+commands = read_commands_data(file_commands)
+counter = 1
+for i in range(len(commands)):
+    if (commands[i])[0] == 'search':
+        file.write("00"+str(counter))
+        file.write(commands[i][0]+" "+decode(commands[i][1])+"\n")
+        file.write('organism'+'protein''\n')
+        file.write(search_data(commands[i]))
+        file.write(mode((commands[i])[1])+'\n')
+        file.write("-------------------------------------------------------------------------------------\n")
+        counter+=1
+
+        if commands[i][0]=="mode":
+            file.write("00"+str(counter)+"  ")
+            file.write(commands[i][0]+"  "+commands[i][1]+"  "+commands[i][2]+"\n")
+            file.write('amino-acids difference:\n')
+            file.write(str(diff(commands[i][1],commands[i][2]))+'\n')
+            file.write("-------------------------------------------------------------------------------------\n")
+            counter+=1
+
 
 
 
