1cc99ab3a343cb478428c39ccca827afb8c752e4
Максим Бартель <mkcmbartel907@gmail.com>
2025-12-04 20:11:14 +0300

    добавил две функции с чтением данных из файла

---
+++ Lab6/A2.0.py
@@ -0,0 +1,44 @@
+import xml.etree.ElementTree as ET
+
+
+def load_users_data():
+ try:
+  users_tree = ET.parse('users.xml')
+  users = []
+  for user_elem in users_tree.getroot().findall('user'):
+    user = {
+      'user_id': int(user_elem.find('user_id').text),
+      'name': user_elem.find('name').text,
+      'age': int(user_elem.find('age').text),
+      'weight': int(user_elem.find('weight').text),
+      'fitness_level': user_elem.find('fitness_level').text,
+      'workouts': []
+    }
+    users.append(user)
+  return users
+ except FileNotFoundError:
+  print("Файл не найден")
+ return []
+
+
+def load_workouts_data():
+  try:
+      workouts_tree = ET.parse('workouts.xml')
+      workouts = []
+      for workout_elem in workouts_tree.getroot().findall('workout'):
+        workout = {
+              'workout_id': int(workout_elem.find('workout_id').text),
+              'user_id': int(workout_elem.find('user_id').text),
+              'date': workout_elem.find('date').text,
+              'type': workout_elem.find('type').text,
+              'duration': int(workout_elem.find('duration').text),
+              'distance': float(workout_elem.find('distance').text),
+              'calories': int(workout_elem.find('calories').text),
+              'avg_heart_rate': int(workout_elem.find('avg_heart_rate').text),
+              'intensity': workout_elem.find('intensity').text
+         }
+        workouts.append(workout)
+      return workouts
+  except FileNotFoundError:
+      print(" Файл  не найден!")
+      return []
